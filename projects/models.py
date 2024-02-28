f0ed03e5f90b68e247a294b989d6e5b99eb9589f
Patrick Plasterer <patrickplasterer@gmail.com>
2024-02-27 20:00:38 -0800

    Added images

--- projects/models.py
+++ projects/models.py
@@ -12,6 +12,7 @@ class Project(models.Model):
     date_created = models.DateField(default=timezone.now())
     technology = models.CharField(max_length=20)
     status = models.CharField(max_length=1, choices=PROJECT_STATUS_CHOICES, default="I")
+    image = models.FileField(upload_to="project_images/", blank=True)
 
     def __str__(self):
         return self.title
